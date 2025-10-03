2358acb0518b5f065af467081fb65dc71304513a
souhail <souhaibadaoui@gmail.com>
2025-10-03 16:53:23 +0100

    simplonacademy

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-    async function  greatWeather() {
-            try{
-                const appid = "b8b57ed6301d03452f8ee1cb956d625d" ;
-                const url =  `https://api.openweathermap.org/data/2.5/weather?q=Taghazout&appid=${appid}&units=metric`;
-        
-            const response = await fetch(url);
-            const data = await response.json();
-              document.getElementById("weather").innerText =
-          `Temp: ${data.main.temp}°C | ${data.weather[0].description}`;
-           }
-          catch(error){
-          console.log(error);
-          }
-          
-        }
+ 
+  async function getWeather() {
+    const apiKey = "b8b57ed6301d03452f8ee1cb956d625d";
+    const city = "Taghazout";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      document.getElementById("temp").innerText = `${Math.round(data.main.temp)}°C`;
+      document.getElementById("desc").innerText = data.weather[0].description;
+      document.getElementById("wind").innerHTML = `<i class="fa-solid fa-wind"></i> Wind: ${data.wind.speed} km/h`;
+      document.getElementById("humidity").innerHTML = `<i class="fa-solid fa-droplet"></i> Humidity: ${data.main.humidity}%`;
+
+     
+      const iconCode = data.weather[0].icon;
+      const iconUrl = `${iconCode}`;
+      document.getElementById("weather-icon").innerHTML = `<img src="${iconUrl}" alt="weather icon">`;
+
+    } catch (error) {
+      
+      document.getElementById("desc").innerText = "Error loading weather ❌";
+    }
+
+  }
+  getWeather();
+  
+  const toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav-item");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+
